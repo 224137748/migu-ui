e107166d30ae7345f303f005d17c314d4bb65534
zhangjunrui <zhangjunrui@beisen.com>
2021-12-25 18:49:40 +0800

    完成button基础样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import MenuDemo from './components/menu/demo/'
+import React from "react";
+import Button, { ButtonType, ButtonSize } from "./components/Button/button";
+
 function App() {
-    return (
-        <div className='App'>
-            <MenuDemo />
-        </div>
-    )
+  return (
+    <div className="App">
+      <Button disabled>hello </Button>
+      <Button btnType={ButtonType.Danger} size={ButtonSize.Large}>
+        Hello
+      </Button>
+      <Button disabled btnType={ButtonType.Link} href="http://www.baidu.com">
+        百度链接
+      </Button>
+    </div>
+  );
 }
 
-export default App
+export default App;
